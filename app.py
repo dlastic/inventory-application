93f58c7c4f391d662dc3721e67fa43e70fecb45b
dlastic <daniellastic@gmail.com>
2025-09-23 13:50:49 +0200

    Create app.py with basic route definitions

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+
+from db.queries import (
+    get_all_categories,
+    get_all_products,
+    get_category_by_id,
+    get_product_by_id,
+    get_products_by_category,
+)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/categories")
+def list_categories():
+    categories = get_all_categories()
+    return render_template("categories.html", categories=categories)
+
+
+@app.route("/categories/<int:category_id>")
+def view_category(category_id):
+    category = get_category_by_id(category_id)
+    products = get_products_by_category(category_id)
+    return render_template("category.html", category=category, products=products)
+
+
+@app.route("/products")
+def list_products():
+    products = get_all_products()
+    return render_template("products.html", products=products)
+
+
+@app.route("/products/<int:product_id>")
+def view_product(product_id):
+    product = get_product_by_id(product_id)
+    return render_template("product.html", product=product)
