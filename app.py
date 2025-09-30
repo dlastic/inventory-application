bf5b961ed0047a841de05035d8cfb2f61a10a164
dlastic <daniellastic@gmail.com>
2025-09-30 10:51:02 +0200

    Edit app to call queries via namespace and rename new_category route

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 from flask import Flask, redirect, render_template, request, url_for
 
-from db.queries import (
-    add_category,
-    get_all_categories,
-    get_all_products,
-    get_category_by_id,
-    get_product_by_id,
-    get_products_by_category,
-)
+from db import queries
 
 app = Flask(__name__)
 
@@ -19,34 +12,34 @@ def index():
 
 @app.route("/categories")
 def list_categories():
-    categories = get_all_categories()
+    categories = queries.get_all_categories()
     return render_template("categories.html", categories=categories)
 
 
 @app.route("/categories/<int:category_id>")
 def view_category(category_id):
-    category = get_category_by_id(category_id)
-    products = get_products_by_category(category_id)
+    category = queries.get_category_by_id(category_id)
+    products = queries.get_products_by_category(category_id)
     return render_template("products.html", category=category, products=products)
 
 
 @app.route("/categories/add", methods=["GET", "POST"])
-def new_category():
+def add_category():
     if request.method == "POST":
         name = request.form["name"]
         description = request.form["description"]
-        add_category(name, description)
+        queries.add_category(name, description)
         return redirect(url_for("list_categories"))
     return render_template("add_category.html")
 
 
 @app.route("/products")
 def list_products():
-    products = get_all_products()
+    products = queries.get_all_products()
     return render_template("products.html", products=products)
 
 
 @app.route("/products/<int:product_id>")
 def view_product(product_id):
-    product = get_product_by_id(product_id)
+    product = queries.get_product_by_id(product_id)
     return render_template("product.html", product=product)
