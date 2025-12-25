70bbdac1db8bb03327a364b12a8674cce42625e6
daniel <daniellastic@gmail.com>
2025-12-25 11:40:05 +0100

    Rewrite connection module to enable sqlite foreign keys

--- db/connection.py
+++ db/connection.py
@@ -1,12 +1,20 @@
 import os
+import sqlite3
 
 from dotenv import load_dotenv
-from sqlalchemy import Engine, create_engine
+from sqlalchemy import create_engine, event
+
+load_dotenv()
+db_url = os.getenv("DB_URL")
+if db_url is None:
+    raise ValueError("DB_URL environment variable not set")
+
+engine = create_engine(db_url)
 
 
-def get_engine() -> Engine:
-    load_dotenv()
-    db_url = os.getenv("DB_URL")
-    if db_url is None:
-        raise ValueError("DB_URL environment variable not set")
-    return create_engine(db_url)
+@event.listens_for(engine, "connect")
+def enable_foreign_keys(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, sqlite3.Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
