15d91566a03684b94b4244bfbd7e21bb0a16d415
daniel <daniellastic@gmail.com>
2025-12-25 12:05:29 +0100

    Define ORM models for category and product

---
+++ db/models.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+from sqlalchemy import ForeignKey, Integer, Numeric, String, Text, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Category(Base):
+    __tablename__ = "categories"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True)
+    description: Mapped[str] = mapped_column(Text)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), onupdate=func.now()
+    )
+    products: Mapped[list["Product"]] = relationship(
+        back_populates="category", passive_deletes=True
+    )
+
+
+class Product(Base):
+    __tablename__ = "products"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True)
+    description: Mapped[str] = mapped_column(Text)
+    price: Mapped[Numeric] = mapped_column(Numeric(10, 2))
+    stock: Mapped[int] = mapped_column(default=0)
+    category_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("categories.id", ondelete="SET DEFAULT"),
+        server_default="1",
+        default=1,
+    )
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(), onupdate=func.now()
+    )
+    category: Mapped[Category] = relationship(back_populates="products")
