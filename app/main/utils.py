6e661f7b0bee4828dabd4aec229567704e781458
dlastic <daniellastic@gmail.com>
2026-01-07 12:00:31 +0100

    Update admin check to abort with 403

--- app/main/utils.py
+++ app/main/utils.py
@@ -1,14 +1,13 @@
 from functools import wraps
 
-from flask import flash, redirect, session, url_for
+from flask import abort, session
 
 
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not session.get("is_admin"):
-            flash("You need admin privileges to access this page.", "error")
-            return redirect(url_for("main.login"))
+            abort(403)
         return f(*args, **kwargs)
 
     return decorated_function
