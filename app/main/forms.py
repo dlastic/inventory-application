27825667bb6e9ce7973308b11dd0d7504f1ac868
dlastic <daniellastic@gmail.com>
2026-03-03 12:53:02 +0100

    Use PasswordField for password inputs

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import PasswordField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    password = StringField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
 
 
 class DeleteForm(FlaskForm):
